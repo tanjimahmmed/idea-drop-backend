dcdd3b186d0f76251aa072beafa5b616bf4c3cd1
Md Tanjim Ahmmed <44721685+tanjimahmmed@users.noreply.github.com>
2025-09-06 20:27:19 +0600

    add api endpoint

--- server.js
+++ server.js
@@ -9,6 +9,24 @@ const PORT = process.env.PORT || 8000;
 
 
 app.use(cors());
+app.use(express.json());
+app.use(express.urlencoded({extended: true}));
+
+app.get('/api/ideas', (req, res) => {
+    const ideas = [
+        {id: 1, title: 'Idea 1', description: 'This is idea 1'},
+        {id: 2, title: 'Idea 2', description: 'This is idea 2'},
+        {id: 3, title: 'Idea 3', description: 'This is idea 3'},
+    ];
+
+    res.json(ideas);
+});
+
+app.post('/api/ideas', (req, res) => {
+    const {title, description} = req.body;
+
+    res.send(title)
+})
 
 app.listen(PORT, () => {
     console.log(`Server is listening on port ${PORT}`);
