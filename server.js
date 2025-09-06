17be434c5f9bbdd8a3d7ad9bb05eb7e25acf50dd
Md Tanjim Ahmmed <44721685+tanjimahmmed@users.noreply.github.com>
2025-09-06 20:03:22 +0600

    Idea Drop Backend Project setup

---
+++ server.js
@@ -0,0 +1,15 @@
+import express from 'express';
+import cors from 'cors';
+import dotenv from 'dotenv';
+
+dotenv.config();
+
+const app = express();
+const PORT = process.env.PORT || 8000;
+
+
+app.use(cors());
+
+app.listen(PORT, () => {
+    console.log(`Server is listening on port ${PORT}`);
+})
