c714e73886dfb1b02276e30a40f36c6725ec5a20
Md Tanjim Ahmmed <44721685+tanjimahmmed@users.noreply.github.com>
2025-09-08 17:22:00 +0600

    add use refresh token

--- routes/authRoutes.js
+++ routes/authRoutes.js
@@ -1,5 +1,7 @@
 import express from 'express';
 import User from '../models/User.js';
+import { jwtVerify } from 'jose';
+import { JWT_SECRET } from '../utils/getJwtSecret.js';
 import { generateToken } from '../utils/generateToken.js';
 
 
@@ -96,7 +98,7 @@ router.post('/login', async (req, res, next) => {
                 email: user.email
             }
         });
-    } catch(error) {
+    } catch(err) {
         console.log(err);
         next(err);
     }
@@ -112,5 +114,44 @@ router.post('/logout', (req, res) => {
 
     res.status(200).json({message: 'Logged out successfully'});
 });
+
+// refresh / generate new access token from refresh token
+// Public / Need valid refresh token in cookie
+router.post('/refresh', async(req, res, next) => {
+    try {
+        const token = req.cookies?.refreshToken;
+        console.log('Refresh Token...')
+
+        if(!token){
+            res.status(401);
+            throw new Error('No refresh token')
+        }
+
+        const {payload} = await jwtVerify(token, JWT_SECRET);
+
+        const user = await User.findById(payload.userId);
+
+        if(!user) {
+            res.status(400);
+            throw new Error('No user')
+        }
+
+        const newAccessToken = await generateToken({userId: user._id.toString()}, '1m');
+
+        res.json({
+            accessToken: newAccessToken,
+            user: {
+                id: user._id,
+                name: user.name,
+                email: user.email
+            }
+        })
+
+    }catch(err){
+        console.log(err);
+        next(err);
+    }
+
+})
 
 export default router;
